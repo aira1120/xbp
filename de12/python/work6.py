e2370105c9d41e822d93e1d65962035bde2f3ff2
aira1120 <r202201723dz@jindai.jp>
2022-11-15 14:07:07 +0900

    1

--- de12/python/work6.py
+++ de12/python/work6.py
@@ -1,6 +1,22 @@
-import random
-
-number = random.randint(1,99) #1~99のランダムな整数。
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <title>XBPのページ</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <h1>ゲーム作成</h1>
+<div>
+    <h2>詳細</h2>
+    グループでは、確率の原理を使ったゲームを作ることにしました。<br>
+    実行ボタンを押すと、「あたり」「はずれ」のどちらかの結果が表示される仕組みです。<br>
+    また、よりゲーム性を持たせるため、当選内容や当選確率の変更が容易にできると良いのではないかと考えました。
+</div>
+<div>
+    <h2>ソースコード</h2>
+    <pre><code>
+    import random
+    number = random.randint(1,99) #1~99のランダムな整数。
 if number == 1:         
     print("121　はずれ")
 elif number == 3:     
@@ -24,8 +40,4 @@ elif number == 99:
 elif number == 76:       
     print("787　はずれ") 
 elif number == 88:       
-    print("676　はずれ") 
-elif number == 91:       
-    print("232　はずれ") 
-else:
-    print("はずれ")
+    print("676　はずれ")...
